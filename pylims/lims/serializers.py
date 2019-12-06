997e459b31322ca281ff8ade4b2876046e823305
kurtw555 <kurt.wolfe@gmail.com>
2019-12-06 14:56:32 -0500

    Initial commit

---
+++ pylims/lims/serializers.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import User, Group, Permission
+from rest_framework import serializers
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name']
+
+
+class PermissionSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Permission
+        fields = ['name', 'content_type', 'codename']
+
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ['url', 'name']
