c6b6b9f053c7276794623254b4d276f0c0ea0fdf
kurtw555 <kurt.wolfe@gmail.com>
2019-12-18 16:43:28 -0500

    Added post method to processor and workflows

--- lims/api.py
+++ lims/api.py
@@ -11,11 +11,13 @@ from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
+from rest_framework import status
 
 import json
 
 from lims.models import Task, Workflow, Processor
 from lims.plugins.plugin_collection import PluginCollection
+from lims.serializers import WorkflowSerializer, TaskSerializer
 
 
 @csrf_exempt
@@ -58,22 +60,41 @@ def users(request):
     return HttpResponse(resultJson, content_type='application/json')
 
 
+@api_view(['GET', 'POST'])
 def tasks(request):
     """
-    API endpoint that returns all tasks.
+    API endpoint that returns all tasks or creates a task
     """
-    result = Task.objects.all()
-    resultJson = serializers.serialize('json', result)
-    return HttpResponse(resultJson, content_type='application/json')
+    if request.method == 'GET':
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data)
+        #resultJson = serializers.serialize('json', result)
+        #return HttpResponse(resultJson, content_type='application/json')
+    elif request.method == 'POST':
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
+@api_view(['GET', 'POST'])
 def workflows(request):
     """
-    API endpoint that returns all workflows
+    API endpoint that returns all workflows or creates new workflow
     """
-    result = Workflow.objects.all()
-    resultJson = serializers.serialize('json', result)
-    return HttpResponse(resultJson, content_type='application/json')
+    if request.method == 'GET':        
+        workflows = Workflow.objects.all()
+        serializer = WorkflowSerializer(workflows, many=True)
+        return Response(serializer.data)
+        #resultJson = serializers.serialize('json', result)
+        #return HttpResponse(resultJson, content_type='application/json')
+    elif request.method == 'POST':
+        serializer = WorkflowSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 def processors(request):
