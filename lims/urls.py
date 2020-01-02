745c3ad58813ce5f8c6c16ec91d0dcab877c38a2
jasondunken <jasondunken@yahoo.com>
2020-01-02 11:40:49 -0500

    remove unsued paths from api

--- lims/urls.py
+++ lims/urls.py
@@ -3,9 +3,6 @@ from django.urls import path
 from lims import api
 
 urlpatterns = [
-    path('register', api.register, name='register'),
-    path('login', api.login, name='login'),
-    path('logout', api.logout, name='logout'),
     path('users/', api.users, name='users'),
     path('tasks/', api.tasks, name='tasks'),
     path('workflows/', api.workflows, name='workflows'),
